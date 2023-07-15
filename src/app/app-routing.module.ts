1e9253df15ff6a17daf5f7064880e36cbc5b38b8
dferreto <diferretomo@est.utn.ac.cr>
2023-07-14 20:33:41 -0600

    Entrega del laboratorio de angular con Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './seguridad/login/login.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [{
+  path: '',
+  redirectTo: 'seguridad/login',
+  pathMatch: 'full',
+},
+{
+  path: 'seguridad/login',
+  component: LoginComponent,
+},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { SeguridadModule } from './seguridad/seguridad.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SeguridadModule
   ],
   providers: [],
   bootstrap: [AppComponent]
